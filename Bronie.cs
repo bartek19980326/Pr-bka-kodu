a7fc09a962a562621d69556f5ca1968446ca0a93
bartek19980326 <65656085+bartek19980326@users.noreply.github.com>
2022-06-01 21:24:32 +0200

    Kod

---
+++ Bronie.cs
@@ -0,0 +1,119 @@
+﻿
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bronie : MonoBehaviour
+{
+  [SerializeField]
+  protected ParticleSystem Wystrzal;
+  [SerializeField]
+  protected GameObject impactEffect;
+  [SerializeField]
+  protected Camera fpscamera;
+  [SerializeField]
+  protected Animator anim;
+  private Wynik wynik;
+  [SerializeField]
+  protected float damage;
+  [SerializeField]
+  protected float szybkostrzelnosc;
+  [SerializeField]
+  protected float range;
+  [SerializeField]
+  protected float impactforce;
+  [SerializeField]
+  protected float nextTimeToFire;
+  [SerializeField]
+  protected float reloadTime;
+  [SerializeField]
+  public int maxAmmo;
+  [SerializeField]
+  public int currentAmmo;
+  [SerializeField]
+  protected bool isReloading;
+  [SerializeField]
+  public Text CurrentAmmoText;
+  [SerializeField]
+  public Text MaxAmmoText;
+  [SerializeField]
+  public Text skocznik;
+  public AudioClip Dzwiek;
+  public AudioClip Przeladowanie;
+  private int PktZaTrafieni = 10;
+  public Wynik pkt;
+
+  public virtual void Start()
+  {
+    this.currentAmmo = this.maxAmmo;
+    this.Dzwiek = Graczmenager.instance.dzwiek;
+    this.Przeladowanie = Graczmenager.instance.Przeladowanie;
+  }
+
+  public virtual void Update()
+  {
+    if (this.isReloading)
+      return;
+    if (this.currentAmmo <= 0)
+    {
+      this.StartCoroutine(this.Reload());
+      AudioManager.Instance.Play(this.Przeladowanie);
+    }
+    else if (this.currentAmmo < this.maxAmmo && Input.GetKey("r"))
+    {
+      this.StartCoroutine(this.Reload());
+      AudioManager.Instance.Play(this.Przeladowanie);
+    }
+    if (!Input.GetButton("Fire1") || (double) Time.time < (double) this.nextTimeToFire)
+      return;
+    this.nextTimeToFire = Time.time + 1f / this.szybkostrzelnosc;
+    this.Shoot();
+    AudioManager.Instance.Play(this.Dzwiek);
+  }
+
+  public virtual void Shoot()
+  {
+    this.Wystrzal.Play();
+    --this.currentAmmo;
+    RaycastHit hitInfo;
+    if (!Physics.Raycast(this.fpscamera.transform.position, this.fpscamera.transform.forward, out hitInfo, this.range))
+      return;
+    Object.Destroy((Object) Object.Instantiate<GameObject>(this.impactEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal)), 2f);
+    ai component1 = hitInfo.transform.GetComponent<ai>();
+    AILucznik component2 = hitInfo.transform.GetComponent<AILucznik>();
+    Debug.Log((object) hitInfo.transform.name);
+    if ((Object) component1 != (Object) null)
+    {
+      component1.GetComponent<stats>().takeDamage(this.damage);
+      this.pkt.ZliczPKT(this.PktZaTrafieni);
+      component1.lookRadius = 60f;
+    }
+    if (!((Object) component2 != (Object) null))
+      return;
+    component2.GetComponent<StatsLucznik>().takeDamage(this.damage);
+    this.pkt.ZliczPKT(this.PktZaTrafieni);
+  }
+
+  public virtual void OnEnable()
+  {
+    this.isReloading = false;
+    this.anim.SetBool("Przeladowanie", false);
+  }
+
+  public IEnumerator Reload()
+  {
+    this.isReloading = true;
+    this.anim.SetBool("Przeladowanie", true);
+    yield return (object) new WaitForSeconds(this.reloadTime);
+    this.anim.SetBool("Przeladowanie", false);
+    this.currentAmmo = this.maxAmmo;
+    this.isReloading = false;
+  }
+
+  public virtual void ZliczAmunicje()
+  {
+    this.CurrentAmmoText.text = this.currentAmmo.ToString();
+    this.MaxAmmoText.text = this.maxAmmo.ToString();
+  }
+}
---
+++ Snajperka.cs
@@ -0,0 +1,96 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class Snajperka : Bronie
+{
+  [SerializeField]
+  private GameObject celownikSnajpera;
+  [SerializeField]
+  private Camera mainCamera;
+  [SerializeField]
+  private GameObject celownik;
+  [SerializeField]
+  private float normalFOV;
+  [SerializeField]
+  private GameObject kameraBroni;
+  [SerializeField]
+  private float FOV;
+  [SerializeField]
+  private bool isScoped;
+  [SerializeField]
+  private new AudioClip Dzwiek;
+  public new AudioClip Przeladowanie;
+
+  public override void Start()
+  {
+    base.Start();
+    this.CurrentAmmoText = Graczmenager.instance.CurrentAmmo;
+    this.fpscamera = Graczmenager.instance.GunCamera;
+    this.MaxAmmoText = Graczmenager.instance.maxammo;
+    this.impactEffect = Graczmenager.instance.Uderzenie;
+    this.anim = Graczmenager.instance.Bron;
+    this.pkt = Graczmenager.instance.wynik;
+    this.celownikSnajpera = Graczmenager.instance.CelownikSnajpera;
+    this.mainCamera = Graczmenager.instance.MainCamera;
+    this.celownik = Graczmenager.instance.Celowanik;
+    this.kameraBroni = Graczmenager.instance.GunCameraObject;
+    this.Dzwiek = Graczmenager.instance.dzwiek;
+    this.Przeladowanie = Graczmenager.instance.Przeladowanie;
+  }
+
+  public override void Update()
+  {
+    if (this.isReloading)
+      return;
+    if (this.currentAmmo <= 0)
+    {
+      this.StartCoroutine(this.Reload());
+      AudioManager.Instance.Play(this.Przeladowanie);
+    }
+    else if ((double) this.currentAmmo < 10.0 && Input.GetKey("r"))
+    {
+      this.StartCoroutine(this.Reload());
+      AudioManager.Instance.Play(this.Przeladowanie);
+    }
+    if (Input.GetButtonDown("Fire1") && (double) Time.time >= (double) this.nextTimeToFire)
+    {
+      this.nextTimeToFire = Time.time + 1f / this.szybkostrzelnosc;
+      this.Shoot();
+      AudioManager.Instance.Play(this.Dzwiek);
+    }
+    this.Scope();
+    this.ZliczAmunicje();
+  }
+
+  public void Scope()
+  {
+    if (!Input.GetButtonDown("Fire2"))
+      return;
+    this.isScoped = this.anim.GetBool("Scoped");
+    this.isScoped = !this.isScoped;
+    this.anim.SetBool("Scoped", this.isScoped);
+    if (this.isScoped)
+      this.StartCoroutine(this.onScoped());
+    else
+      this.onUnscoped();
+  }
+
+  public IEnumerator onScoped()
+  {
+    yield return (object) new WaitForSeconds(0.15f);
+    this.celownikSnajpera.SetActive(true);
+    this.kameraBroni.SetActive(false);
+    this.celownik.SetActive(false);
+    this.normalFOV = this.mainCamera.fieldOfView;
+    this.mainCamera.fieldOfView = this.FOV;
+  }
+
+  public void onUnscoped()
+  {
+    this.celownikSnajpera.SetActive(false);
+    this.celownik.SetActive(true);
+    this.kameraBroni.SetActive(true);
+    this.mainCamera.fieldOfView = 60f;
+  }
+}
